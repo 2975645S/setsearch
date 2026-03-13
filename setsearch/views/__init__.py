ab9acc3ae2dd0460621c86435a8df4f8059aa33e
2975645S <newtykip@gmail.com>
2026-03-13 17:17:34 +0000

    refactor: views

---
+++ setsearch/views/__init__.py
@@ -0,0 +1,20 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from setsearch.models import Artist, Concert, Comment
+
+
+def home_page(request: HttpRequest) -> HttpResponse:
+    return render(request, "home.html")
+
+
+def view_artist(request: HttpRequest, artist_slug: str) -> HttpResponse:
+    # todo: handle 404 (done)
+    artist = get_object_or_404(Artist, slug=artist_slug)
+    return render(request, "artist.html", {"artist": artist})
+
+
+def view_concert(request: HttpRequest, concert_id: str) -> HttpResponse:
+    concert = get_object_or_404(Concert, id=concert_id)
+    comments = Comment.objects.filter(concert=concert).order_by("timestamp")
+    return render(request, "concert.html", {"concert": concert, "comments": comments})

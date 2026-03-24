7936e91db301e1687221cc873a1fb5805288e6c9
2975645S <newtykip@gmail.com>
2026-03-24 16:23:46 +0000

    feat: show total artist attendee count

--- setsearch/views/artist.py
+++ setsearch/views/artist.py
@@ -4,7 +4,9 @@ from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_GET
 
 from setsearch.forms.artist import ArtistLinkForm
-from setsearch.models import Artist, Concert, Song
+from setsearch.models import Artist, Concert, Song, Attendance
+
+SONG_COUNT = 5
 
 
 @require_GET
@@ -13,7 +15,10 @@ def view_artist(request: HttpRequest, artist_slug: str) -> HttpResponse:
     artist = get_object_or_404(Artist, slug=artist_slug)
     concerts = Concert.objects.filter(artist=artist).order_by("-date")
     songs = Song.objects.filter(artist=artist).annotate(play_count=Count("setlistentry")).filter(
-        play_count__gt=0).order_by("-play_count")[:5]
+        play_count__gt=0).order_by("-play_count")[:SONG_COUNT]
+    attendee_count = Attendance.objects.filter(concert__artist=artist).count()
     form = ArtistLinkForm(artist)
 
-    return render(request, "artist.html", {"artist": artist, "concerts": concerts, "songs": songs, "form": form})
+    return render(request, "artist.html",
+                  {"artist": artist, "concerts": concerts, "songs": songs, "attendee_count": attendee_count,
+                   "form": form})
